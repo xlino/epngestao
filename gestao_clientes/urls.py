5137959bb8b7200e494909e60f36ef7060ff0fec
ermelino <epiazzetta@gmail.com>
2022-01-25 10:19:22 -0300

    Funcionando CRUD

--- gestao_clientes/urls.py
+++ gestao_clientes/urls.py
@@ -13,12 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#from django.contrib import admin
+#from django.urls import path
+#from clientes import urls as clientes_urls
+#from django.urls import path, include
+
+#urlpatterns = [
+#    path('clientes/', include(clientes_urls)),
+#    path('admin/', admin.site.urls),
+#]
 from django.contrib import admin
-from django.urls import path
-from clientes import urls as clientes_urls
 from django.urls import path, include
+from clientes import urls as clientes_urls
+from home import urls as home_urls
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', include(home_urls)),
     path('clientes/', include(clientes_urls)),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
