65c32ffb05f2bfcaf8d75aa92e5dca3ce12d7438
ermelino <epiazzetta@gmail.com>
2022-01-26 14:13:50 -0300

    commit com as alterações para upload para heroku

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from decouple import config
+from dj_database_url import parse as dburl
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-glrty8u3t2t8ug2hmyap*kl-fy)c@l!4%f2%zd16r#8+d_xo$s'
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['epngestao.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -76,22 +78,25 @@ WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gestao_clientes',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'gestao_clientes',
+#        'USER': 'postgres',
+#        'PASSWORD': '123',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 
 
 # Password validation
@@ -133,5 +138,10 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'person_list'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #TATICFILES_DIRS = ['statics']
+# Activate Django-Heroku.
+django_heroku.settings(locals())
